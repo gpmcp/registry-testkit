068d784681b74548675e90c7ef45043706b66c2a
Sandipsinh Dilipsinh Rathod <62684960+ssddOnTop@users.noreply.github.com>
2025-12-05 22:21:32 -0500

    chore(ci): remove accidentally committed build artifacts

--- ci/target/debug/deps/ci-2f5d65bf242e937a.d
+++
@@ -1,5 +0,0 @@
-/home/ssdd/RustroverProjects/registry-testkit/ci/target/debug/deps/ci-2f5d65bf242e937a.d: src/main.rs
-
-/home/ssdd/RustroverProjects/registry-testkit/ci/target/debug/deps/libci-2f5d65bf242e937a.rmeta: src/main.rs
-
-src/main.rs:
--- ci/target/debug/deps/ci-c25825ff4884e2fc.d
+++
@@ -1,5 +0,0 @@
-/home/ssdd/RustroverProjects/registry-testkit/ci/target/debug/deps/ci-c25825ff4884e2fc.d: src/main.rs
-
-/home/ssdd/RustroverProjects/registry-testkit/ci/target/debug/deps/libci-c25825ff4884e2fc.rmeta: src/main.rs
-
-src/main.rs:
